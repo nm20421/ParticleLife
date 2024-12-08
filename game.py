1de51cacd186f434dec0726fcdfedf00a37f8f23
Edward Horton <74405702+nm20421@users.noreply.github.com>
2024-12-08 15:15:26 +0000

    Spatial Partitioning and JIT compiler
    To speed up the calculation time per step (and so allow for more partidles to be simulated) two optimisations have been introduced.
    1. A spatial partitioning method has been implemented so each particle has less iterations to perform. This doesn't give a much of a speed up as I expected, although this may be due to rendering now instead of  calculations.
    2. The code has been majorly adjusted to allow for the just-in-time (JIT) compiler to be used.

--- game.py
+++ game.py
@@ -3,94 +3,164 @@ import pygame
 import numpy as np
 import random
 import math 
+from numba import jit, int32, float32
+from numba.experimental import jitclass
+from scripts.entities import *
 
-from scripts.entities import Particle
+
+def fps_counter(display):
+    font = pygame.font.SysFont("Arial" , 18 , bold = True)
+    fps = str(int(clock.get_fps()))
+    fps_t = font.render(fps , 1, pygame.Color("RED"))
+    display.blit(fps_t,(0,0))
+
+def flatten(xss):
+    return [x for xs in xss for x in xs]
+
+def run(x_res,y_res):
+    while True:
+    #self.display.blit(self.assets['background'],[0,0])
+        display.fill((0,0,0))
 
 
-class Game:
+
+        for part in range(0,len(particle_list)):
+
+            #This need changnig to only add in grids
+            rangeG = 1
+            local_particles = []
+            g_row = int(particle_list[part,3])
+            g_col = int(particle_list[part,4])
+
+            for row in range(-1,2):
+                for col in range(-1,2):
+                    #Wrap around:
+                    row_val =int((row+g_row+y_res)%y_res)
+                    col_val =int((col+g_col+x_res)%x_res)
+                    if row_val >= grid_rows:
+                        row_val = 0
+                    if col_val >= grid_cols:
+                        col_val = 0
+                    local_particles.append(grid[row_val][col_val])
+                    #print(row,col)
+            local_particles = flatten(local_particles)
+            local_particles = np.array(local_particles)
+
+            pos = update(part,particle_list,local_particles,attraction_matrix,x_res,y_res)
+            particle_list[part,0:2] = pos
+            render(display,particle_list[part])
+
+            #now use pos to remove particle from old grid and add to new grid.
+            new_g_row = int(math.floor(pos[1]/interaction_range))
+            new_g_col = int(math.floor(pos[0]/interaction_range))
+
+            g_row = new_g_row
+            g_col = new_g_col
+
+
+
+        for event in pygame.event.get():
+            #event is an input of some description
+            #CLose window event
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        fps_counter(display)
+        #Scale up screen
+        screen.blit(pygame.transform.scale(display, screen.get_size()),(0,0))
+        pygame.display.update()
+        #Force game to run at 60 FPS
+        clock.tick(60)
+
+
+
+
+
+
+
+
     # This function allows us to call screen and clock in any future functions containing (self) as an initialisation
-    def __init__(self):
-        pygame.init()
+pygame.init()
 
         #Create screen
         #Set windows name
-        pygame.display.set_caption("Particle Life!")
-        #640,480 = resolution
-        self.x_res = 1000
-        self.y_res = 700
-        self.screen = pygame.display.set_mode((self.x_res,self.y_res))
+pygame.display.set_caption("Particle Life!")
+# resolution of window
+x_window = 1400
+y_window = 1000
+
+#scale of game. >1 means smaller pixels, <1 means bigger pixels
+scale = 1
+
+
+x_res = x_window*scale
+y_res = y_window*scale
+screen = pygame.display.set_mode((x_window,y_window))
 
         # To scale up the screen we render at a smaller size then scale it up.
         #Create a black box with 320,240 size.
-        self.display = pygame.Surface((1000,700))
+display = pygame.Surface((x_res,y_res))
 
         #self.particle = Particle(self,(1,1),)
 
         #set max FPS
-        self.clock = pygame.time.Clock()
+clock = pygame.time.Clock()
         #Tilemap load
         #self.tilemap = Tilemap(self,tile_size=20)
 
         #Camera
-        self.scroll = [0,0]
-
-        self.n_particles = 200
-        self.particle_list = np.zeros([self.n_particles,3])
-        self.n_particle_types = 3
 
 
-        self.particle_list = []
-        for particle in range(0,self.n_particles):
-            position = [int(random.random()*self.x_res),int(random.random()*self.y_res)]
-            ptype = int(random.random()*self.n_particle_types)
+n_particles = 2000
+particle_list = np.zeros([n_particles,5])
+n_particle_types = 5
 
-            self.particle_list.append(Particle(self,position,ptype))
+
+#define sub-regions
+interaction_range = 100
+
+grid_rows = int(y_res/interaction_range)
+grid_cols = int(x_res/interaction_range)
+
+#create list of lists
+grid = [ [] for _ in range(grid_rows)]
+for row in range(0,grid_rows):
+    grid[row] = [ [] for _ in range(grid_cols)]
+
+
+#create particvles
+for particle in range(0,n_particles):
+    position = [int(random.random()*x_res),int(random.random()*y_res)]
+    ptype = int(random.random()*n_particle_types)
+
+    #Find grid that each particle is in and add to list of lists
+    g_row = int(math.floor(position[1]/interaction_range))
+    g_col = int(math.floor(position[0]/interaction_range))
+    grid[g_row][g_col].append(particle)
+
+    particle_list[particle,0] = position[0]
+    particle_list[particle,1] = position[1]
+    particle_list[particle,2] = ptype 
+    particle_list[particle,3] = g_row 
+    particle_list[particle,4] = g_col 
         
         #set up attraction matrix
-        self.attraction_matrix = np.zeros([self.n_particle_types,self.n_particle_types])
+    attraction_matrix = np.zeros([n_particle_types,n_particle_types])
 
-        for row in range(0,self.n_particle_types):
-            for col in range(0,self.n_particle_types):
-                if row == col:
-                    self.attraction_matrix[row,col] = 1
-                else:
-                    self.attraction_matrix[row,col] = random.random()*(1-(-1))+-1
-        print(self.attraction_matrix)
-
-
-    def fps_counter(self):
-        font = pygame.font.SysFont("Arial" , 18 , bold = True)
-        fps = str(int(self.clock.get_fps()))
-        fps_t = font.render(fps , 1, pygame.Color("RED"))
-        self.display.blit(fps_t,(0,0))
+for row in range(0,n_particle_types):
+    for col in range(0,n_particle_types):
+        if row == col:
+            attraction_matrix[row,col] = 1#*random.random()*(1-(-1))+-1
+        else:
+            attraction_matrix[row,col] = 1*random.random()*(1-(-1))+-1
+print(attraction_matrix)
 
 
 
-    def run(self):
-        while True:
-        #self.display.blit(self.assets['background'],[0,0])
-            self.display.fill((0,0,0))
-
-
-            for particle in self.particle_list:
-                particle.update(self.particle_list,self.attraction_matrix)
-                particle.render(self.display)
 
 
 
-            for event in pygame.event.get():
-                #event is an input of some description
-                #CLose window event
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+#run simulation
+run(x_res,y_res)
 
-            self.fps_counter()
-            #Scale up screen
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()),(0,0))
-            pygame.display.update()
-            #Force game to run at 60 FPS
-            self.clock.tick(60)
-
-
-Game().run()
--- scripts/entities.py
+++ scripts/entities.py
@@ -2,100 +2,130 @@ import pygame
 import math
 import random
 import numpy as np
+from numba import jit, int32, float32
+from numba import cuda
 
 
-class Particle:
-    def __init__(self,game,pos,ptype):
-        self.game = game
-        self.pos = list(pos)
-        self.ptype = ptype
-        self.movement = (0,0)
-        self.velocity = [0,0]
-        self.interaction_range = 100
-    
-    def update(self,particle_list,attraction_matrix):
-        p1pos = self.pos
+
+@jit(nopython=True,cache=True)
+def update(part,particle_list,local_particles,attraction_matrix,x_res,y_res):
+    p1pos = particle_list[part,0:2]
+    ptype1 = int(particle_list[part,2])
+
+    pos = p1pos
+    mid_range = 51.5
+    min_range = 15
+    interaction_range = 100
 
 
-        p_forces =[]   
-    #look through list of particles and find any within the interaction range
-        self.velocity[0] = 0
-        self.velocity[1] = 0
-        runstuff = 1
-        if runstuff == 1:
-            for particle in particle_list:
-                
-                #position
-                p2pos = particle.pos
-                #if itself then ignore
-                if p2pos == p1pos:
-                    continue
-                
-                #get distance.
-                distance = np.sqrt((p2pos[0]-p1pos[0])**2+(p2pos[1]-p1pos[1])**2)
-                #distance = 200
-                if distance > self.interaction_range:
-                    continue
-                else:
-                    #within interaction range
+#look through list of particles and find any within the interaction range
+    velocity = [0,0]
+    f_total = [0,0]
 
-                    #Find the vector
-                    vector = [p2pos[0]-p1pos[0],p2pos[1]-p1pos[1]]
-                    norm = np.linalg.norm(vector)
-                    vec_norm = vector/norm
-
-                    #calc relative force. This is a function of distance AND the attraction matrix.
-                    attraction = attraction_matrix[self.ptype,particle.ptype]
-
-                    #If too close push away:
-                    if distance < 3:
-                        force = vec_norm*-1
-                    else:
-                        if distance <= 50:
-                            f_mag =  (1/47*distance - 3/47)*attraction
-                        elif distance > 50:
-                            f_mag = (-1/47*distance+1+50/47)*attraction
-                        force = vec_norm*f_mag
-                    p_forces.append(force)
+    p_forces = []
+    runstuff = 1
+    if runstuff == 1:
+        for particle in local_particles:
             
-            f_total = sum(p_forces)
+            #position
+            p2pos = particle_list[particle,0:2]
 
-            if len(p_forces) > 0:
-                self.velocity[0] = f_total[0]
-                self.velocity[1] = f_total[1]
+            #p2pos[0] = (p2pos[0]+x_res)%x_res
+            #p2pos[1] = (p2pos[1]+y_res)%y_res
+            ptype2 = int(particle_list[particle,2])
+            #if itself then ignore
+            if p2pos[0] == p1pos[0] and p2pos[0] == p1pos[0]:
+                continue
+            
+            #get distance.
+            distance = np.sqrt((p2pos[0]-p1pos[0])**2+(p2pos[1]-p1pos[1])**2)
+            #distance = 200
+            if distance > interaction_range:
+                continue
             else:
-                self.velocity[0] = random.random()
-                self.velocity[1] = random.random()
+                #within interaction range
 
+                #Find the vector
+                vector = np.array([p2pos[0]-p1pos[0],p2pos[1]-p1pos[1]], dtype=np.float64)
+                #vector = [p2pos[0]-p1pos[0],p2pos[1]-p1pos[1]]
+                norm = np.linalg.norm(vector)
+                vec_norm = vector/norm
 
-            #periodic boundaries:
-            if self.pos[0] > self.game.x_res:
-                self.pos[0] = 1.0
-            elif self.pos[0] < 0:
-                self.pos[0] = self.game.x_res-1
-            
-            if self.pos[1] > self.game.y_res:
-                self.pos[1] = 1.0
-            elif self.pos[1] < 0:
-                self.pos[1] = self.game.y_res-1
+                #calc relative force. This is a function of distance AND the attraction matrix.
+                attraction = attraction_matrix[ptype1,ptype2]
 
-        self.pos[0] = self.pos[0] + self.velocity[0]
-        self.pos[1] = self.pos[1] + self.velocity[1]
+                #If too close push away:
+                if distance < min_range:
+                    force = vec_norm*-1
+                else:
+                    if distance <= interaction_range/2:
+                        f_mag =  (1/(mid_range-min_range)*distance - min_range/(mid_range-min_range))*attraction
+                    elif distance > interaction_range/2:
+                        f_mag = (-1/(mid_range-min_range)*distance+1+mid_range/(mid_range-min_range))*attraction
+                    force = vec_norm*f_mag
+                p_forces.append(force)
+        a = len(p_forces)
+        p_f = np.zeros((len(p_forces),2))
 
+        for i in range(0,len(p_f)):
+            p_f[i,0] = p_forces[i][0]
+            p_f[i,1] = p_forces[i][1]
+        #p_forces = np.array(p_forces,dtype=np.float64)
 
+        if len(p_forces) != 0:
+            f_total[0] = np.sum(p_f[:,0])
+            f_total[1] = np.sum(p_f[:,1])
 
-
-    def render(self,surf):
-        if self.ptype == 0:
-            colour = (255,0,0)
-        elif self.ptype == 1:
-            colour = (0,255,0)
-        elif self.ptype == 2:
-            colour = (0,0,255)
+        if len(p_forces) > 0:
+            velocity[0] = f_total[0]
+            velocity[1] = f_total[1]
         else:
-            colour = (255,255,255)
+            velocity[0] = random.random()*19
+            velocity[1] = random.random()*10
 
-        shape = pygame.Rect(self.pos[0],self.pos[1],5,5)
         
-        pygame.draw.rect(surf,colour,shape)
-        #pygame.draw.circle(surf,colour,self.pos,3)
+        #periodic boundaries:
+        #if pos[0] > x_res:
+        #    pos[0] = 1.0 + velocity[0]
+        #elif pos[0] < 0:
+        #    pos[0] = x_res-1 + velocity[0]
+        
+        #if pos[1] > y_res:
+        #    pos[1] = 1.0 + velocity[0]
+        #elif pos[1] < 0:
+        #    pos[1] = y_res-1 + velocity[0]
+
+    pos[0] = pos[0] + velocity[0]
+    pos[1] = pos[1] + velocity[1]
+
+
+
+    return pos
+
+
+
+
+def render(surf,particle):
+    ptype = particle[2]
+    pos = particle[0:2]
+    if ptype == 0:
+        colour = (255,0,0)
+    elif ptype == 1:
+        colour = (0,255,0)
+    elif ptype == 2:
+        colour = (0,0,255)
+    elif ptype == 3:
+        colour = (243,100,100)
+    elif ptype == 4:
+        colour = (0,100,255)
+    elif ptype == 5:
+        colour = (255,0,255)
+    elif ptype == 6:
+        colour = (200,0,55)
+    else:
+        colour = (0,0,0)
+
+    shape = pygame.Rect(pos[0],pos[1],5,5)
+    
+    pygame.draw.rect(surf,colour,shape)
+    #pygame.draw.circle(surf,colour,self.pos,3)
